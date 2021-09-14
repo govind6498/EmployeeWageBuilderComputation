a0b207257de24a8ca9679b4806e99776e98ddbcc
govind6498 <govindsingh6498@gmail.com>
2021-09-14 12:38:18 +0530

    Added:To Check EMployee are present or not

--- src/com/bridgelavz/employeewagebuildercomputation/EmployeeWageBuilder.java
+++ src/com/bridgelavz/employeewagebuildercomputation/EmployeeWageBuilder.java
@@ -3,5 +3,13 @@ package com.bridgelavz.employeewagebuildercomputation;
 public class EmployeeWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		int IS_FULL_TIME=1;
+		double empCheck=Math.floor(Math.random() *10)%2;
+		if(empCheck==IS_FULL_TIME) {
+			System.out.println("Employee is Present");
+		}
+		else {
+			System.out.println("Employee is Abscent");
+		}
 	}
 }
