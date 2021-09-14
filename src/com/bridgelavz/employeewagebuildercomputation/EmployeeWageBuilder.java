9ef2244261e45ba3ab9ac25ee83ff2c824ab7a0b
govind6498 <govindsingh6498@gmail.com>
2021-09-14 12:50:21 +0530

    Added:To Add Part Time Employee and  Wage

--- src/com/bridgelavz/employeewagebuildercomputation/EmployeeWageBuilder.java
+++ src/com/bridgelavz/employeewagebuildercomputation/EmployeeWageBuilder.java
@@ -2,13 +2,17 @@ package com.bridgelavz.employeewagebuildercomputation;
 
 public class EmployeeWageBuilder {
 	public static void main(String[] args) {
-		int IS_FULL_TIME=1;
+		int IS_PART_TIME=1;
+		int IS_FULL_TIME=2;
 		int EMP_RATE_PER_HOUR=20;
 		int empHrs=0;
 		int empWage=0;
-		
-		double empCheck = Math.floor(Math.random()*10)%2;
-		if(empCheck==IS_FULL_TIME) {
+
+		double empCheck = Math.floor(Math.random()*10)%3;
+		if(empCheck==IS_PART_TIME) {
+			empHrs=4;
+		}
+		else if(empCheck==IS_FULL_TIME) {
 			empHrs=8;
 		}
 		else {
@@ -17,4 +21,5 @@ public class EmployeeWageBuilder {
 		empWage=empHrs*EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage:"+empWage);
 	}
+
 }
