6d319131eb02554f76059ac329073b089ba04c17
govind6498 <govindsingh6498@gmail.com>
2021-09-14 12:57:42 +0530

    Added:To Calculating wage per Month

--- src/com/bridgelavz/employeewagebuildercomputation/EmployeeWageBuilder.java
+++ src/com/bridgelavz/employeewagebuildercomputation/EmployeeWageBuilder.java
@@ -4,23 +4,30 @@ public class EmployeeWageBuilder {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int NUM_OF_WORKING_DAYS=2;
 	public static void main(String[] args) {
 		int empHrs=0;
 		int empWage=0;
-
-		int  empCheck =(int) Math.floor(Math.random()*10)%3;
-		switch(empCheck) {
-		case IS_PART_TIME:
-			empHrs=4;
-			break;
-		case IS_FULL_TIME:
-			empHrs=8;
-			break;
-		default:
-			empHrs=0;
+		int totalEmpWage=0;
+		for(int day=0;day<=NUM_OF_WORKING_DAYS;day++){
+			int empCheck =(int) Math.floor(Math.random()*10)%3;
+			switch(empCheck) {
+			case IS_PART_TIME:
+				empHrs=4;
+				break;
+			case IS_FULL_TIME:
+				empHrs=8;
+				break;
+			default:
+				empHrs=0;
+			}
+			empWage=empHrs*EMP_RATE_PER_HOUR;
+			totalEmpWage+=empWage;
+			System.out.println("Emp Wage:"+empWage);
 		}
-		empWage=empHrs*EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage:"+empWage);
+		System.out.println("Total Emp Wage:"+totalEmpWage);
+
 	}
+
 
 }
