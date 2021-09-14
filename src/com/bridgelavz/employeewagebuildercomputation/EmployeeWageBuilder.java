012e7ad4daca85ebb7254ce242cd51fcda3a338e
govind6498 <govindsingh6498@gmail.com>
2021-09-14 13:01:44 +0530

    Added:To Calculate wage till a condition of total working hours or days in reached for a month

--- src/com/bridgelavz/employeewagebuildercomputation/EmployeeWageBuilder.java
+++ src/com/bridgelavz/employeewagebuildercomputation/EmployeeWageBuilder.java
@@ -5,11 +5,11 @@ public class EmployeeWageBuilder {
 	public static final int IS_FULL_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORKING_DAYS=2;
+	public static final int MAX_HRS_IN_MONTH=10;
 	public static void main(String[] args) {
-		int empHrs=0;
-		int empWage=0;
-		int totalEmpWage=0;
-		for(int day=0;day<=NUM_OF_WORKING_DAYS;day++){
+		int empHrs=0,totalEmpHrs=0,totalWorkingDays=0;
+		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
 			int empCheck =(int) Math.floor(Math.random()*10)%3;
 			switch(empCheck) {
 			case IS_PART_TIME:
@@ -21,10 +21,10 @@ public class EmployeeWageBuilder {
 			default:
 				empHrs=0;
 			}
-			empWage=empHrs*EMP_RATE_PER_HOUR;
-			totalEmpWage+=empWage;
-			System.out.println("Emp Wage:"+empWage);
+			totalEmpHrs+=empHrs;
+			System.out.println("Days:"+totalWorkingDays +" Emp Hr:"+empHrs);
 		}
+		int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage:"+totalEmpWage);
 
 	}
