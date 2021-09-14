835b78755460464dea766213f85d7a997ff1876b
govind6498 <govindsingh6498@gmail.com>
2021-09-14 12:53:54 +0530

    Added:TO Solve Using  Switch case Statement

--- src/com/bridgelavz/employeewagebuildercomputation/EmployeeWageBuilder.java
+++ src/com/bridgelavz/employeewagebuildercomputation/EmployeeWageBuilder.java
@@ -1,21 +1,22 @@
 package com.bridgelavz.employeewagebuildercomputation;
 
 public class EmployeeWageBuilder {
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
 	public static void main(String[] args) {
-		int IS_PART_TIME=1;
-		int IS_FULL_TIME=2;
-		int EMP_RATE_PER_HOUR=20;
 		int empHrs=0;
 		int empWage=0;
 
-		double empCheck = Math.floor(Math.random()*10)%3;
-		if(empCheck==IS_PART_TIME) {
+		int  empCheck =(int) Math.floor(Math.random()*10)%3;
+		switch(empCheck) {
+		case IS_PART_TIME:
 			empHrs=4;
-		}
-		else if(empCheck==IS_FULL_TIME) {
+			break;
+		case IS_FULL_TIME:
 			empHrs=8;
-		}
-		else {
+			break;
+		default:
 			empHrs=0;
 		}
 		empWage=empHrs*EMP_RATE_PER_HOUR;
