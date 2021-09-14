00eedc170e8108505477950017b716973e33bd64
govind6498 <govindsingh6498@gmail.com>
2021-09-14 12:43:10 +0530

    Added:To Calculate daily Wage Employee

--- src/com/bridgelavz/employeewagebuildercomputation/EmployeeWageBuilder.java
+++ src/com/bridgelavz/employeewagebuildercomputation/EmployeeWageBuilder.java
@@ -2,14 +2,19 @@ package com.bridgelavz.employeewagebuildercomputation;
 
 public class EmployeeWageBuilder {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 		int IS_FULL_TIME=1;
-		double empCheck=Math.floor(Math.random() *10)%2;
+		int EMP_RATE_PER_HOUR=20;
+		int empHrs=0;
+		int empWage=0;
+		
+		double empCheck = Math.floor(Math.random()*10)%2;
 		if(empCheck==IS_FULL_TIME) {
-			System.out.println("Employee is Present");
+			empHrs=8;
 		}
 		else {
-			System.out.println("Employee is Abscent");
+			empHrs=0;
 		}
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage:"+empWage);
 	}
 }
