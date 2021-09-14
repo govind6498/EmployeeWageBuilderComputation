d29e8e3f5a38193146caddfcde1f7456fe95d2f3
govind6498 <govindsingh6498@gmail.com>
2021-09-14 12:32:58 +0530

    Added:To Display Welcome to Employee Wage Computation Program on Master Branch

---
+++ src/com/bridgelavz/employeewagebuildercomputation/EmployeeWageBuilder.java
@@ -0,0 +1,7 @@
+package com.bridgelavz.employeewagebuildercomputation;
+
+public class EmployeeWageBuilder {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+	}
+}
