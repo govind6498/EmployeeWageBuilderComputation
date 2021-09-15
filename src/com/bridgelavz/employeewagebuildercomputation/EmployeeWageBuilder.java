2fe6e5364bc0da0f2dc7b6ab981cdafbaeffa8dd
govind6498 <govindsingh6498@gmail.com>
2021-09-15 15:22:46 +0530

    Added:To add Multiple Companies

--- src/com/bridgelavz/employeewagebuildercomputation/EmployeeWageBuilder.java
+++ src/com/bridgelavz/employeewagebuildercomputation/EmployeeWageBuilder.java
@@ -3,30 +3,30 @@ package com.bridgelavz.employeewagebuildercomputation;
 public class EmployeeWageBuilder {
 	public static final int FULL_TIME=1;
 	public static final int PART_TIME=2;
-	public static final int EMP_RATE_PER_DAYS=20;
-	public static final int NUM_OF_WORKING_DAYS=20;
-	public static final int MAX_HRS_IN_MONTH=100;
-	public static void main(String[] args) {
-		double empHours=0,totalEmpHrs=0,totalWorkingDays=0;
-		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS) {
+	public static int computeEmployeWage(String company,int empRateHour,int numOfWorkingDays,int maxHourPerMonth) {
+		int empHrs=0,totalEmpHrs=0,totalWorkingDays=0;
+		while(totalEmpHrs<=maxHourPerMonth && totalWorkingDays<numOfWorkingDays) {
 			totalWorkingDays++;
-			int empCheck=(int)Math.floor(Math.random()*10)%3;
+			int empCheck=(int) Math.floor(Math.random()*10)%3;
 			switch(empCheck) {
 			case FULL_TIME:
-				empHours=8;
+				empHrs=8;
 				break;
 			case PART_TIME:
-				empHours=4;
+				empHrs=4;
 				break;
 			default:
-				empHours=0;
+				empHrs=0;
 			}
-			totalEmpHrs+=empHours;
-			System.out.println("Day "+totalWorkingDays+" -Hours worked:"+empHours);
+			totalEmpHrs+=empHrs;
+			System.out.println("Days: "+totalWorkingDays +" Emp Hr: "+empHrs);
 		}
-		int totalEmpWage=(int) (totalEmpHrs*EMP_RATE_PER_DAYS);
-		System.out.println("Total Emp Wage:"+totalEmpWage);
+		int totalEmpWage=totalEmpHrs*empRateHour;
+		System.out.println("Total Emp Wage for Company: "+company+" is: "+totalEmpWage);
+		return totalEmpWage;
 	}
-
-
+	public static void main(String[] args) {
+		computeEmployeWage("VMart",20,2,10);
+		computeEmployeWage("AMAZON",10,5,25);
+	}
 }
