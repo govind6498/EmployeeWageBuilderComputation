e2f4a5248f744c140e5bec4e03b17c4f327e54b8
govind6498 <govindsingh6498@gmail.com>
2021-09-15 12:58:05 +0530

    REFECTOR:Method to Compute Employee Wage

--- src/com/bridgelavz/employeewagebuildercomputation/EmployeeWageBuilder.java
+++ src/com/bridgelavz/employeewagebuildercomputation/EmployeeWageBuilder.java
@@ -1,32 +1,31 @@
 package com.bridgelavz.employeewagebuildercomputation;
 
 public class EmployeeWageBuilder {
-	public static final int IS_PART_TIME=1;
-	public static final int IS_FULL_TIME=2;
-	public static final int EMP_RATE_PER_HOUR=20;
-	public static final int NUM_OF_WORKING_DAYS=2;
-	public static final int MAX_HRS_IN_MONTH=10;
+	public static final int FULL_TIME=1;
+	public static final int PART_TIME=2;
+	public static final int EMP_RATE_PER_DAYS=20;
+	public static final int NUM_OF_WORKING_DAYS=20;
+	public static final int MAX_HRS_IN_MONTH=100;
 	public static void main(String[] args) {
-		int empHrs=0,totalEmpHrs=0,totalWorkingDays=0;
+		double empHours=0,totalEmpHrs=0,totalWorkingDays=0;
 		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS) {
 			totalWorkingDays++;
-			int empCheck =(int) Math.floor(Math.random()*10)%3;
+			int empCheck=(int)Math.floor(Math.random()*10)%3;
 			switch(empCheck) {
-			case IS_PART_TIME:
-				empHrs=4;
+			case FULL_TIME:
+				empHours=8;
 				break;
-			case IS_FULL_TIME:
-				empHrs=8;
+			case PART_TIME:
+				empHours=4;
 				break;
 			default:
-				empHrs=0;
+				empHours=0;
 			}
-			totalEmpHrs+=empHrs;
-			System.out.println("Days:"+totalWorkingDays +" Emp Hr:"+empHrs);
+			totalEmpHrs+=empHours;
+			System.out.println("Day "+totalWorkingDays+" -Hours worked:"+empHours);
 		}
-		int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
+		int totalEmpWage=(int) (totalEmpHrs*EMP_RATE_PER_DAYS);
 		System.out.println("Total Emp Wage:"+totalEmpWage);
-
 	}
 
 
